3abe29f6c97d5010cbcaeca4383e498011d2fc2f
Alexander Carr <1alexander.carr@gmail.com>
2023-12-20 19:22:17 +0000

    login page tests include sign in click

--- final-project/cypress/e2e/homepage.cy.js
+++ final-project/cypress/e2e/homepage.cy.js
@@ -5,11 +5,13 @@ describe('Visit Homepage, Login Input Field', () => {
 })
 
 describe('Login Field, Password Field selected, filled with login details', () => {
-  it('gets the login input field by its placeholder, then types in an email. Then it does the same for the password field', () => {
+  it('gets the login input field by its placeholder, then types in an email. Then it does the same for the password field. The sign in page is then clicked, and url for dashboard is verified', () => {
     cy.visit('https://shelterapp-homehorizon.onrender.com/');
 
     cy.get('input[placeholder="Your email address"]').type('1alexander.carr@gmail.com');
     cy.get('input[placeholder="Your password"]').type('abcdef');
+    cy.contains('Sign in').click()
+    cy.url().should('include', 'https://shelterapp-homehorizon.onrender.com/')
   })
 })
 
