efbd85476161e7a6f3fe265613cad16bec645b41
Sara Thampi <saraannthampi@gmail.com>
2024-01-04 20:53:08 +0000

    adding zod and updating toasts

--- final-project/src/app/pagecomponents/AddSUComp.jsx
+++ final-project/src/app/pagecomponents/AddSUComp.jsx
@@ -1,16 +1,13 @@
 import compStyles from "./addsu.css";
-// import { createClient } from "@supabase/supabase-js";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 // Make sure to npm i to install Toast package
 import { toast } from "sonner";
 import { supabase } from "../AuthRouter";
+import { z } from "zod";
 
-// const supaurl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supakey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-//uses the supabase client to connect to supabase
-// const supabase = createClient(supaurl, supakey);
+
 //, last_name:"", age:"", gender:"", dob:"", ni_number:"", phone:"", emergency_contact_name:"", emergency_contact_relationship:"", email:"", emergency_contact_phone:""
 //, last_name, age, gender, dob, ni_number, phone, emergency_contact_name, emergency_contact_relationship, email, emergency_contact_phone
 
@@ -27,13 +24,10 @@ export default function AddSUComp({ staffId, staffName }) {
   }, []);
   // INPUT SETUP ________________________________________________________________
   // let userIDForAdding = ""
-
-  // input- profile _____________________
-  // 1 blank columns
   const profileColumnsBlank = {
     first_name: "",
     last_name: "",
-    age: "",
+    age: undefined,
     gender: "",
     dob: "",
     ni_number: "",
@@ -46,6 +40,24 @@ export default function AddSUComp({ staffId, staffName }) {
     emergency_contact_phone: "",
     su_image: "",
   };
+  
+  const profileColumnsSchema = z.object({
+    first_name: z.string({ message: "First Name is required" }),
+    last_name: z.string({ message: "Last Name is required" }),
+    age: z.number().min(18).max(120).optional(),
+    gender: z.string().optional(),
+    dob: z.date().optional(),
+    ni_number: z.string().length(9).optional(),
+    phone: z.string().regex(/^\d{11}$/, { message: "Must contain exactly 11 numbers" }).optional(),
+    accomodation_needs: z.string().optional(),
+    languages: z.string().optional(),
+    emergency_contact_name: z.string().optional(),
+    emergency_contact_relationship: z.string().optional(),
+    email: z.string().email().optional(),
+    emergency_contact_phone: z.string().regex(/^\d{11}$/, { message: "Must contain exactly 11 numbers" }).optional(),
+    su_image: z.string().optional(),
+  });
+  
   // 2 input state
   const [inputProfile, setInputProfile] = useState(profileColumnsBlank);
   // 3 destructuring input state
@@ -82,6 +94,8 @@ export default function AddSUComp({ staffId, staffName }) {
     emergency_contact_phone,
     su_image,
   };
+
+
 
   // input- strengths _____________________
   // 1 blank columns
@@ -190,17 +204,67 @@ export default function AddSUComp({ staffId, staffName }) {
     previous_stays,
   };
 
+  // Function to reset all input fields
+const resetInputFields = () => {
+  setInputProfile(profileColumnsBlank); 
+  // setInputMedical(medicalColumnsBlank);
+  // setInputEmploymentStatus(employmentStatusColumnsBlank);
+  // setInputComments(commentsColumnsBlank);
+  // setInputResidence(residenceColumnsBlank);
+};
+  
+
   // SUBMIT POST FUNCTION
   async function submitPost(tableName, columns, columnsBlank, isProfile) {
+    try{
     const { data, error } = await supabase
       .from(tableName)
       .insert(columns)
       .single()
       .select();
+      const suName = data.first_name + " " + data.last_name;
+      let success = true;
+      toast("Success", {
+        className: "submit-toast",
+        description: `Service User ${suName} Successfully Added`,
+        duration: 3000,
+        position: "top-left",
+        // onAutoClose: window.location.reload(), //will reload page(after toast disappears)
+        style: {
+          background: "#B3F5A3",
+          color: "black",
+          border: "3px solid white",
+        },
+        
+      });
+      // This function will refresh the page after 4 seconds, clearing the input fields
+      //setTimeout(function () {
+        // Refresh the page
+        //window.location.reload();
+      //}, 4000); // Adjust the delay time
+      //instead have chosen to reset the fields using state
+      resetInputFields()
+
     // if(isProfile) {userIDForAdding = data.user_id};
     // console.log(userIDForAdding)
     setFetchedDataProfile(columnsBlank);
     fetchDataProfile();
+    } catch {
+    console.error("error creating record")
+    toast("Error", {
+      className: "error-toast",
+      description: `Could not create service user`,
+      duration: 3000,
+      position: "top-left",
+      //onAutoClose: window.location.reload(), //will reload page(after toast disappears)
+      style: {
+        background: "#B3F5A3",
+        color: "red",
+        border: "3px solid white",
+      },
+      
+    });
+    }
   }
  
 
@@ -209,7 +273,7 @@ export default function AddSUComp({ staffId, staffName }) {
       {/* WELCOME BOX */}
       <section className="global-welcome">
           <h1 className="global-heading">Add a new service user</h1>
-          <p className="global-description">You can then edit their full profile in the database.</p>
+          <p className="global-description">You can then edit their full profile in the database</p>
         </section>
       {/* CONTENT BOX */}
       <section className="global-content">
@@ -270,7 +334,7 @@ export default function AddSUComp({ staffId, staffName }) {
             name="age"
             min={18}
             max={120}
-            value={age}
+            value={age ?? ''}
             onChange={(e) =>
               setInputProfile({ ...inputProfile, age: e.target.value })
             }
@@ -306,7 +370,7 @@ export default function AddSUComp({ staffId, staffName }) {
             min={"1900-01-01"}
             max={"2040-12-18"}
             className="addsu-inputfield"
-            id="dob_input"
+            id="dob"
             value={dob}
             onChange={(e) =>
               setInputProfile({ ...inputProfile, dob: e.target.value })
@@ -337,11 +401,32 @@ export default function AddSUComp({ staffId, staffName }) {
 
         </div>
 
+        {/* PROFILE - phone */}
+        <div className="addsu-inputcontainer">
+          <label htmlFor="phone" id="phone_label" className="addsu-labeltext">
+            Phone:{" "}
+          </label>
+          <input
+            placeholder="Phone"
+            value={phone}
+            type="text"
+            className="addsu-inputfield"
+            id="phone"
+            minLength={11}
+            maxLength={11}
+            pattern="[0-9]{11}"
+            onChange={(e) =>
+              setInputProfile({ ...inputProfile, phone: e.target.value })
+            }
+          />
+
+        </div>
+
                 {/* PROFILE - accomodation needs  */}
                 <div className="addsu-inputcontainer">
           <label
-            htmlFor="accomodation needs"
-            id="accomodation needs"
+            htmlFor="accomodation_needs"
+            id="accomodation-needs-label"
             className="addsu-labeltext"
           >
             Accomodation needs:{" "}
@@ -349,7 +434,7 @@ export default function AddSUComp({ staffId, staffName }) {
           <input
             type="text"
             placeholder="Accomodation needs"
-            id="accomodation needs"
+            id="accomodation_needs"
             className="addsu-inputfield"
             value={accomodation_needs}
             onChange={(e) =>
@@ -365,7 +450,7 @@ export default function AddSUComp({ staffId, staffName }) {
                 <div className="addsu-inputcontainer">
           <label
             htmlFor="languages"
-            id="languages"
+            id="languages_input"
             className="addsu-labeltext"
           >
             Languages:{" "}
@@ -375,6 +460,7 @@ export default function AddSUComp({ staffId, staffName }) {
             placeholder="Languages"
             id="languages"
             className="addsu-inputfield"
+            name="languages"
             value={languages}
             onChange={(e) =>
               setInputProfile({
@@ -483,43 +569,52 @@ export default function AddSUComp({ staffId, staffName }) {
           onClick={async function (e) {
             // the e.preventDefault() prevents the page from refreshing when the button is clicked
             e.preventDefault();
-            e.target.disabled = true;
 
+           //prepare data for zod validation
+            const inputData = {
+              first_name,
+              last_name,
+              age: age ? parseInt(age, 10) : null, //converts string value to number decimal(base 10) or null
+              gender,
+              dob: dob ? new Date(dob) : null, //input value is a string, this converts it to a date or null
+              ni_number: ni_number.length === 0 ? null : ni_number,
+              phone: phone.length === 0 ? null : phone,
+              accomodation_needs,
+              languages,
+              email: email.length === 0 ? null : email,
+              emergency_contact_name,
+              emergency_contact_relationship,
+              emergency_contact_phone: emergency_contact_phone.length === 0 ? null : emergency_contact_phone,
+            };
+            console.log("Input Data:", inputData);
+            //this filters out the null values or it will not pass zod validation
+            const filteredInputData = Object.fromEntries(
+              Object.entries(inputData).filter(([_, value]) => value !== null)
+            );
+            
+        
             try {
+              // Validate the input data using zod
+              const result = profileColumnsSchema.safeParse(filteredInputData);
+              //if the data is not valid the errors will be logged in the console
+              if(!result.success) {
+                console.log("Validation errors:", result.error);
+              }
+              else {
+                //disables the submit button to prevent accidentally submitting data twice
+              e.target.disabled = true;
               await submitPost(
                 "service_users",
                 [profileColumns],
                 [profileColumnsBlank],
                 true
               );
-              //  We are using a package called Toast which has been imported (remember to npm i)
-              // The toast title is "Success" and the description is "Service User Successfully Added"
-              // The notification will remain on screen for 3 seconds
-              // The position is top-left, can style in the style attribute
-              toast("Success", {
-                className: "submit-toast",
-                description: "Service User Successfully Added",
-                duration: 3000,
-                position: "top-left",
-                // onAutoClose: window.location.reload(), //will reload page(after toast disappears)
-                style: {
-                  background: "#B3F5A3",
-                  color: "black",
-                  border: "3px solid white",
-                },
-              });
-
-              // This function will refresh the page after 4 seconds, clearing the input fields
-              setTimeout(function () {
-                // Refresh the page
-                window.location.reload();
-              }, 4000); // Adjust the delay time
-
-              //   alert("Service User Successfully Added");
-              // displaymessage();
+              e.target.disabled = false;
+              }
             } catch (error) {
+              e.target.disabled = false;
               console.error("Error submitting form:", error);
-              // Handle errors if needed
+          
             }
           }}
         >
